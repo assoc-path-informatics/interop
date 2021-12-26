d75ede731c50c50700f8c61cc502709f466caf8e
Noah Hoffman <noah.hoffman@gmail.com>
2021-12-25 16:45:13 -0800

    Squashed commit of the following:
    commit ed00bf50402f649b3acccf60b5c5ef53aaef803d
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 16:38:01 2021 -0800
    
        add template pages to nav
    
    commit cc17a8653d0537f07a4fa8f3012be42fbe5ec1a7
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 16:13:18 2021 -0800
    
        include {{ SITEURL }} in paths in base.html
    
    commit 158001e5855fafcef9042f60c49a01bbcdab630f
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 16:09:53 2021 -0800
    
        make a copy of base.html from Flex
    
    commit 24396b96d1dd3aa9ccd76ceb68855384e5e615af
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 15:33:57 2021 -0800
    
        list static image paths
    
    commit 1deb702087e527d6fd7caa0fb5acdbddfb9c6f7f
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 15:13:37 2021 -0800
    
        try links to images without SITEURL
    
    commit d779247063bef5c21164c60e8434776ff99efd3b
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 15:00:33 2021 -0800
    
        remove elegant submodule
    
    commit 1961895e0180b203dd2633a7114c9859a8410709
    Merge: 026aa8c ca37c56
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Sat Dec 25 14:35:29 2021 -0800
    
        Merge branch 'main' into flex
    
    commit 026aa8c250c31847acec0a3f459649007a986125
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:37:27 2021 -0800
    
        this really seems like it should work
    
    commit acb03b9e9d2acbfc64d149e7b3068bf32dd86657
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:32:46 2021 -0800
    
        fix css, for real this time
    
    commit 81d6ca76aedf4d79702bfbaaafb4cf8acc600b00
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:31:06 2021 -0800
    
        fix custom.css, take 2
    
    commit 6d301b781d992dc17a8f03cca29451e1a2ac0c32
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:28:51 2021 -0800
    
        try to fix url for static.css
    
    commit 8f00cbb687e3d31a3d5b8edc3e1a16a6e59aca32
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:20:35 2021 -0800
    
        fix typo
    
    commit aaffd4ea63e478c93ecab0b01ace190b25b8f80d
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:18:12 2021 -0800
    
        explicitly add SITEURL to asset paths
    
    commit c9060ad1df5d7cae7649f7e6d56bc342b1ca2164
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 08:08:25 2021 -0800
    
        add favicon
    
    commit 2ad46af35677d369841378e915a2350957d2aff0
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 07:13:34 2021 -0800
    
        clean up config for flex theme
    
    commit a225e74e3e25d06be2d9ffc166433935556929d4
    Author: Noah Hoffman <noah.hoffman@gmail.com>
    Date:   Tue Dec 14 06:38:34 2021 -0800
    
        use Flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,8 @@ config = DEFAULT_CONFIG.copy()
 
 AUTHOR = 'Association for Pathology Informatics'
 SITENAME = 'API Interoperability Resources'
+
+# should not end with trailing slash
 SITEURL = 'https://assoc-path-informatics.github.io/interop'
 
 PATH = 'content'
@@ -24,10 +26,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('API home', 'https://www.pathologyinformatics.org/'),
+    ('github issues', 'https://github.com/assoc-path-informatics/interop/issues'),
+)
+
+GITHUB_CORNER_URL = 'https://github.com/assoc-path-informatics/interop'
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -38,14 +42,30 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-THEME = "./elegant"
+# THEME = "./elegant"
+THEME = "./Flex"
+THEME_COLOR = 'light'
 
 # place files replacing theme templates in ./content/templates
 THEME_TEMPLATES_OVERRIDES = ['./content/templates']
-DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search', '404'))
-STATIC_PATHS = ['images']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search'))
 
-# prevent Pelican from reading files matching the following patterns
+# custom variable indicating template links to include in nav
+TEMPLATE_LINKS = ['categories', 'tags', 'archives']
+
+# copied to /output without modification
+STATIC_PATHS = ['images', 'css']
+
+# these resources referenced as href="{{ SITEURL }}/{{ var }}"
+SITELOGO = 'images/API-250px-300dpi.jpg'
+FAVICON = 'images/favicon.ico'
+CUSTOM_CSS = 'css/custom.css'
+
+DISPLAY_PAGES_ON_MENU = True
+# do not append page slug to urls for pages
+DISABLE_URL_HASH = True
+
+# # prevent Pelican from reading files matching the following patterns
 IGNORE_FILES = ['.#*', 'includes', 'templates', 'README.md']
 
 # render markdown contents from files in /content/includes and make
@@ -54,3 +74,4 @@ INCLUDES = {}
 for fname in glob.glob('./content/includes/*.md'):
     pth = Path(fname)
     INCLUDES[pth.stem], _ = MarkdownReader(config).read(fname)
+
